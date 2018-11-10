64a2791a6d7f683fa83e0c1fe8af5f144744242a
Magajanes <thiagosmfis@gmail.com>
2018-11-10 18:03:42 -0200

    Creat Recipe/Ingredient/Instructions scripts, ingredients prefabs and first instruction data file

---
+++ Assets/Scripts/Instructions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewInstructions", menuName = "Recipe/Instructions", order = 0)]
+public class Instructions : ScriptableObject
+{
+    public List<Ingredient> IngredientsList;
+
+    public List<int> MeasuresList;
+}
---
+++ Assets/Scripts/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recipe : MonoBehaviour
+{
+    public string DishName;
+
+    public Dictionary<Ingredient, int> PrepareSteps;
+
+    public Instructions PrepareInstructions;
+}
